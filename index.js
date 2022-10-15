83580dd58bd26669e0ce84cde0d4afc0486f1889
KLeonGreen <114649634+KLeonGreen@users.noreply.github.com>
2022-10-15 18:23:11 +0200

    new option

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-const truebtn = document.getElementById("true");
-const resetbtn = document.getElementById("reset");
-const nextbtn = document.getElementById("next");
-const submitbtn = document.getElementById("submit");
-let falsebtn;
-
-const totalScore = document.getElementById("total-points");
-const questionText = document.getElementById("questions-content");
-
-let currentQuestion = 0;
 const questions = [
   {
     category: "Science: Computers",
@@ -16,6 +6,7 @@ const questions = [
     question: "What does CPU stand for?",
     correct_answer: "Central Processing Unit",
     incorrect_answers: ["Central Process Unit", "Computer Personal Unit", "Central Processor Unit"],
+    right: "4",
   },
   {
     category: "Science: Computers",
@@ -24,23 +15,24 @@ const questions = [
     question: "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn&#039;t get modified?",
     correct_answer: "Final",
     incorrect_answers: ["Static", "Private", "Public"],
+    right: "4",
   },
-  {
-    category: "Science: Computers",
-    type: "boolean",
-    difficulty: "easy",
-    question: "The logo for Snapchat is a Bell.",
-    correct_answer: "False",
-    incorrect_answers: ["True"],
-  },
-  {
-    category: "Science: Computers",
-    type: "boolean",
-    difficulty: "easy",
-    question: "Pointers were not used in the original C programming language; they were added later on in C++.",
-    correct_answer: "False",
-    incorrect_answers: ["True"],
-  },
+  //   {
+  //     category: "Science: Computers",
+  //     type: "boolean",
+  //     difficulty: "easy",
+  //     question: "The logo for Snapchat is a Bell.",
+  //     correct_answer: "False",
+  //     incorrect_answers: ["True"],
+  //   },
+  //   {
+  //     category: "Science: Computers",
+  //     type: "boolean",
+  //     difficulty: "easy",
+  //     question: "Pointers were not used in the original C programming language; they were added later on in C++.",
+  //     correct_answer: "False",
+  //     incorrect_answers: ["True"],
+  //   },
   {
     category: "Science: Computers",
     type: "multiple",
@@ -48,6 +40,7 @@ const questions = [
     question: "What is the most preferred image format used for logos in the Wikimedia database?",
     correct_answer: ".svg",
     incorrect_answers: [".png", ".jpeg", ".gif"],
+    right: "4",
   },
   {
     category: "Science: Computers",
@@ -56,6 +49,7 @@ const questions = [
     question: "In web design, what does CSS stand for?",
     correct_answer: "Cascading Style Sheet",
     incorrect_answers: ["Counter Strike: Source", "Corrective Style Sheet", "Computer Style Sheet"],
+    right: "4",
   },
   {
     category: "Science: Computers",
@@ -64,6 +58,7 @@ const questions = [
     question: "What is the code name for the mobile operating system Android 7.0?",
     correct_answer: "Nougat",
     incorrect_answers: ["Ice Cream Sandwich", "Jelly Bean", "Marshmallow"],
+    right: "4",
   },
   {
     category: "Science: Computers",
@@ -72,15 +67,16 @@ const questions = [
     question: "On Twitter, what is the character limit for a Tweet?",
     correct_answer: "140",
     incorrect_answers: ["120", "160", "100"],
+    right: "4",
   },
-  {
-    category: "Science: Computers",
-    type: "boolean",
-    difficulty: "easy",
-    question: "Linux was first created as an alternative to Windows XP.",
-    correct_answer: "False",
-    incorrect_answers: ["True"],
-  },
+  //   {
+  //     category: "Science: Computers",
+  //     type: "boolean",
+  //     difficulty: "easy",
+  //     question: "Linux was first created as an alternative to Windows XP.",
+  //     correct_answer: "False",
+  //     incorrect_answers: ["True"],
+  //   },
   {
     category: "Science: Computers",
     type: "multiple",
@@ -88,63 +84,62 @@ const questions = [
     question: "Which programming language shares its name with an island in Indonesia?",
     correct_answer: "Java",
     incorrect_answers: ["Python", "C", "Jakarta"],
+    right: "4",
   },
 ];
 
-const startQuiz = function () {
-  currentQuestion = Math.floor(Math.random() * 10);
-  //   totalScore.innerText = currentQuestion;
+let currentQuestion = 0;
+let score = 0;
+const totalQuestions = questions.length;
 
-  questionText.innerHTML = questions[currentQuestion].question;
-  const quest = questions[currentQuestion].question;
-  quest.classList.add("all-questions");
-  truebtn.innerHTML = questions[currentQuestion].correct_answer;
+const quizSpace = document.getElementById("container");
+const questionTitle = document.getElementById("questions-content");
+const userScore = document.getElementById("user-points");
+const totalScore = document.getElementById("total-points");
+const answer1 = document.getElementById("option1");
+const answer2 = document.getElementById("option2");
+const answer3 = document.getElementById("option3");
+const answer4 = document.getElementById("option4");
+const next = document.getElementById("next");
+const result = document.getElementById("result");
+const allradios = document.querySelectorAll("input[name='option']");
 
-  truebtn.addEventListener("onclick", function () {
-    if (questions[currentQuestion].correct_answer === truebtn.innerHTML) {
-      truebtn.innerHTML = "Correct Answer";
-    }
-    if (currentQuestion < 2) {
-      nextQuestions();
-    }
-  });
+totalScore.textContent = questions.length * 10;
 
-  const incorectanswer = questions[currentQuestion].incorrect_answers;
-  const falsebtn = document.createElement("button");
-  for (let i = 0; i < incorectanswer.length; i++) {
-    const buttonContain = document.getElementById("btn-options");
-    buttonContain.appendChild(falsebtn);
-    falsebtn.innerHTML = incorectanswer[i];
+const loadQuestion = function (questionIndex) {
+  const questionTodisplay = questions[questionIndex];
+  questionTitle.textContent = questionTodisplay.question;
+  answer1.textContent = questionTodisplay.incorrect_answers[0];
+  answer2.textContent = questionTodisplay.incorrect_answers[1];
+  answer3.textContent = questionTodisplay.incorrect_answers[2];
+  answer4.textContent = questionTodisplay.correct_answer;
+};
 
-    falsebtn.addEventListener("onlick", function () {});
+const nextQuestion = function () {
+  const selectRadio = document.querySelector("input[type=radio]:checked");
+  if (!selectRadio) {
+    alert("Please Select an answer");
   }
+
+  const correctAnswer = selectRadio.value;
+  if (correctAnswer === questions[currentQuestion].right) {
+    score = score + 10;
+    userScore.textContent = score;
+  }
+  selectRadio.checked = false;
+  currentQuestion++;
+  if (currentQuestion === totalQuestions - 1) {
+    next.textContent = "Finish";
+  }
+  if (currentQuestion === totalQuestions) {
+    quizSpace.style.display = "none";
+    result.style.display = "block";
+    result.textContent = "Bravo" + score;
+  }
+
+  loadQuestion(currentQuestion);
 };
 
-// startQuiz();
+loadQuestion(currentQuestion);
 
-const resetQuestions = function () {
-  currentQuestion = 0;
-  nextbtn.classList.remove("hide");
-  submitbtn.classList.remove("hide");
-  truebtn.classList.remove("hide");
-  //   falsebtn.classList.remove("hide");
-
-  startQuiz();
-};
-
-const nextQuestions = function () {
-  currentQuestion = currentQuestion + 1;
-  startQuiz();
-};
-
-const submitQuestions = function () {
-  nextbtn.classList.remove("hide");
-  submitbtn.classList.remove("hide");
-  truebtn.classList.remove("hide");
-  falsebtn.classList.remove("hide");
-  questionText.innerHTML = "Thank You";
-};
-
-resetbtn.addEventListener("click", resetQuestions);
-nextbtn.addEventListener("click", nextQuestions);
-submitbtn.addEventListener("click", submitQuestions);
+next.addEventListener("click", nextQuestion);
